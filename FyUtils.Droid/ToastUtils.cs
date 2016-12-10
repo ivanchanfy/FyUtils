19750761068e71444ed3ce065aee5360bb1edf18
Ivan.fy <ivanchan.fy@outlook.com>
2016-12-10 21:25:05 +0800

    added ToastUtils

---
+++ FyUtils.Droid/ToastUtils.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Android.Content;
+using Android.Views;
+using Android.Widget;
+
+namespace FyUtils.Droid {
+    public static class ToastUtils {
+        public static void ShowToast(Context context, string message, ToastLength duration = ToastLength.Short) {
+            Toast.MakeText(context, message, duration).Show();
+        }
+
+        public static void ShowToast(Context context, int resId, ToastLength duration = ToastLength.Short) {
+            Toast.MakeText(context, resId, duration).Show();
+        }
+
+        public static void ShowToast(Context context, View view, ToastLength duration = ToastLength.Short) {
+            Toast toast = new Toast(context);
+            toast.View = view;
+            toast.Duration = duration;
+            toast.Show();
+        }
+    }
+}
